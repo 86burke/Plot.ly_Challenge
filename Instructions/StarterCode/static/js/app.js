abae6d66d18a9eb9295b5c141a8f91a26d6c868b
john burke <86burke@gmail.com>
2021-11-23 18:45:42 -0600

    created dataset to search data

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -4,5 +4,8 @@ d3.json("samples.json").then((bbdata) => {
     console.log(bbdata);
     var data = bbdata;
 
-
-    
+    var idDD = data.names;
+    for (var x = 0; x < idDD.length; x++) {
+        selectBox = d3.select("#selDataset");
+        selectBox.append("option").text(idDD[x]);
+    }
